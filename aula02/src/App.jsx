6cd42d6bb3cc36420a322ef68672e010c699a846
leonardomateus10 <leonardomateus131415@gmail.com>
2026-05-12 11:38:26 -0300

    12/05

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -2,13 +2,20 @@
  import Exemplo1 from "./Exemplo1";
  import Exercicio1 from "./exercicio1";
  import Exercicio2 from "./exercicio2";
- import Exercicio3 from "./exercicio3";
+ import Exercicio3 from "./exercicio3";    
+ import Exercicio4 from "./exercicio4";
+ import Exercicio5 from "./exercicio5";
+
  export default function App()
-{
-  return(
-    <div>
+ {
+  return (
+    <div> 
+     
+     
       <h1>aula 02 - criação de componentes</h1>
-      
+
+
+
      <div className="card">
       <h3>chamadas para o componente  esxemplo 1</h3>
 
@@ -24,25 +31,38 @@
       </div>
 
      <div className="card">
-        <h3>chamadas para o componente exercicio2</h3>
+        <h3> exercicio2</h3>
         <Exercicio2  peso ={80} altura ={1.80} />
         <hr />
         <Exercicio2 peso ={60} altura={1.60} />
      </div>
 
+     <div className="card">
+      <h3>exercicio3</h3>
+      <Exercicio3 nota1 ={7.5} nota2={9.5}/>
+      <hr />
+      <Exercicio3 nota1 ={3} nota2={6.5}/>
+      <hr />
+      </div>
+
+      <div className="card">
+      <h3> Exercício 4</h3>
+      <Exercicio4 tipo="retangulo" base={10} altura={5} />
+      <Exercicio4 tipo="triangulo" base={10} altura={5} />
+      <Exercicio4 tipo="quadrado" base={10} altura={10} />
+      
+      </div> 
+
+      <div className="card">
+       <hr />
+      <h3> Exercício 5</h3>
+      <Exercicio5 quantidade={10} />
+      <hr />
+      <Exercicio5 quantidade={20} />
      </div>
-
  
-  
-
-
-  
-
-
-  
-
-
-  )
+      </div> 
+      
     
-
+    )      
 }
