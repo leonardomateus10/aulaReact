032264ec1fe531ad6503a5c58ebfd4ad9eb47eeb
leonardomateus10 <leonardomateus131415@gmail.com>
2026-07-28 11:38:03 -0300

    28/07

---
+++ aula03/src/paginas/EspiritoSanto.jsx
@@ -0,0 +1,8 @@
+export default  function  EspiritoSanto()
+{
+  return(
+    <div>
+      <h1> estado do Espirito Santo</h1>
+    </div>
+  );
+}
---
+++ aula03/src/paginas/Home.jsx
@@ -0,0 +1,8 @@
+export default  function  Home()
+{
+  return(
+    <div>
+      <h1>Aula 03 -Estudos de Rotas</h1>
+    </div>
+  );
+}
